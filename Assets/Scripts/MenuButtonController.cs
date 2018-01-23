a82a6911211fe99b316126594c531f7914e9e59c
Lewis Nicholson <Lewies@Live.co.uk>
2018-01-23 18:52:24 +0000

    Added menu selection

---
+++ Assets/Scripts/MenuButtonController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class MenuButtonController : MonoBehaviour, IPointerEnterHandler// required interface when using the OnPointerEnter method.
+{
+	//Do this when the cursor enters the rect area of this selectable UI object.
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		MenuController.Instance.SetSelected (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuController : MonoBehaviour {
+
+	public static MenuController Instance = null;
+
+	[SerializeField] private EventSystem m_es;
+	private GameObject m_selected;
+
+	private void Awake()
+	{
+		if (Instance == null) {
+			Instance = this;
+		} else if (Instance != this) {
+			Destroy (gameObject);   
+		}
+
+		if (m_es == null) {
+			m_es = GetComponent<EventSystem> ();
+		}
+	}
+
+	private void Start () 
+	{
+		m_selected = m_es.firstSelectedGameObject;
+	}
+	
+
+	private void Update () 
+	{
+		if (m_es.currentSelectedGameObject != m_selected) {
+			if (m_es.currentSelectedGameObject != null) {
+				m_selected = m_es.currentSelectedGameObject;
+			} else {
+				m_es.SetSelectedGameObject(m_selected);
+			}
+		}
+	}
+
+	public void SetSelected(GameObject selected)
+	{
+		m_selected = selected;
+		m_es.SetSelectedGameObject(selected);
+	}
+}
